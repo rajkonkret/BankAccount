e58ea4969f4a941f6e75e1f0c76e7ba21a54fe33
rajkonkret <rajkonkret660@gmail.com>
2019-12-11 08:55:47 +0100

    stram.of

--- src/Run.java
+++ src/Run.java
@@ -1,3 +1,6 @@
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Run {
     public static void main(String[] args) {
         makeBankOperation(Initialization());
@@ -6,16 +9,8 @@ public class Run {
     private static void makeBankOperation(Account... accountsArray) {
         for (int i = 0; i < 10; i++) {
 
-            for (Account account : accountsArray) {
-                System.out.println("Stan: " + String.format("%,.2f", account.accountValue()));
-                System.out.println("wpłacam 100,00");
-                account.addToAccount(100);
-                System.out.println("Stan: " + String.format("%,.2f", account.accountValue()));
-                System.out.println("wypłacam 50,00");
-                account.cashOut(50);
-                System.out.println("Stan: " + String.format("%,.2f", account.accountValue()));
-
-            }
+            Stream.of(accountsArray)
+                    .collect(Collectors.toList()).forEach(a -> printOperation(a));
         }
         for (Account konto : accountsArray) {
             System.out.println("Na koniec mam: " + konto.toPrint());
@@ -29,5 +24,14 @@ public class Run {
         SaverAccount kontoOszczednosciowe = new SaverAccount();
         ExtraAccount extraKonto = new ExtraAccount();
         return new Account[]{superKasa, zycieJakWMadrycie, kontoOszczednosciowe, extraKonto};
+    }
+    private static void printOperation(Account account){
+        System.out.println("Stan: " + String.format("%,.2f", account.accountValue()));
+        System.out.println("wpłacam 100,00");
+        account.addToAccount(100);
+        System.out.println("Stan: " + String.format("%,.2f", account.accountValue()));
+        System.out.println("wypłacam 50,00");
+        account.cashOut(50);
+        System.out.println("Stan: " + String.format("%,.2f", account.accountValue()));
     }
 }
